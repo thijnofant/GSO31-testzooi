fdd62bd5aae4d854a73f078971d1f9509ab5614d
pepperoni <robert.horvers@gmail.com>
2015-03-17 11:36:18 +0100

    blijfnaklj

--- UnittestenTime/src/fontys/time/TimeSpan2.java
+++ UnittestenTime/src/fontys/time/TimeSpan2.java
@@ -15,7 +15,7 @@ public class TimeSpan2 implements ITimeSpan {
     private long length;
     
     public TimeSpan2(ITime bt, ITime et) {
-        if (bt.compareTo(et) <= 0) {
+        if (bt.compareTo(et) >= 0) {
             throw new IllegalArgumentException("begin time "
                     + bt + " must be earlier than end time " + et);
         }
@@ -41,11 +41,19 @@ public class TimeSpan2 implements ITimeSpan {
 
     @Override
     public void setBeginTime(ITime beginTime) {
+        if (beginTime.compareTo(this.getEndTime()) >= 0) {
+            throw new IllegalArgumentException("begin time "
+                    + bt + " must be earlier than end time " + this.getEndTime());
+        }
         this.bt = beginTime;
     }
 
     @Override
     public void setEndTime(ITime endTime) {
+        if (endTime.compareTo(bt) <= 0) {
+            throw new IllegalArgumentException("end time "
+                    + this.getEndTime() + " must be later then begin time " + bt);
+        }
         this.length = this.bt.difference(endTime);
     }
 
@@ -56,26 +64,57 @@ public class TimeSpan2 implements ITimeSpan {
 
     @Override
     public void changeLengthWith(int minutes) {
+        if (minutes <= 0) {
+            throw new IllegalArgumentException("length of period must be positive");
+        }
         this.length += minutes;
     }
 
     @Override
     public boolean isPartOf(ITimeSpan timeSpan) {
-        if (this.bt.compareTo(timeSpan.getBeginTime()) <= 0) {
-            return false;
-        }
-        //if (this.bt.)
-        return false;
+        return (this.bt.compareTo(timeSpan.getBeginTime()) <= 0 && 
+                this.getEndTime().compareTo(timeSpan.getBeginTime()) >= 0);
     }
 
     @Override
     public ITimeSpan unionWith(ITimeSpan timeSpan) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (bt.compareTo(timeSpan.getEndTime()) >= 0 || this.getEndTime().compareTo(timeSpan.getBeginTime()) <= 0) {
+            return null;
+        }
+        
+        ITime begintime, endtime;
+        if (bt.compareTo(timeSpan.getBeginTime()) <= 0) {
+            begintime = bt;
+        } else {
+            begintime = timeSpan.getBeginTime();
+        }
+        if (this.getEndTime().compareTo(timeSpan.getEndTime()) > 0) {
+            endtime = this.getEndTime();
+        } else {
+            endtime = timeSpan.getEndTime();
+        }
+
+        return new TimeSpan(begintime, endtime);
     }
 
     @Override
     public ITimeSpan intersectionWith(ITimeSpan timeSpan) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ITime begintime, endtime;
+        if (bt.compareTo(timeSpan.getBeginTime()) > 0) {
+            begintime = bt;
+        } else {
+            begintime = timeSpan.getBeginTime();
+        }
+        if (this.getEndTime().compareTo(timeSpan.getEndTime()) < 0) {
+            endtime = this.getEndTime();
+        } else {
+            endtime = timeSpan.getEndTime();
+        }
+        if (begintime.compareTo(endtime) >= 0) {
+            return null;
+        }
+
+        return new TimeSpan(begintime, endtime);
     }
     
 }
