29d487a780cfb771d371d5c759230ba902698ec1
thijnofant <tavandijk@live.nl>
2015-03-23 13:36:54 +0100

    woop woop week 6

---
+++ AEX Banner Rober-Thijn/src/aex/banner/rober/thijn/IFonds.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aex.banner.rober.thijn;
+
+/**
+ *
+ * @author Thijn
+ */
+public interface IFonds {
+    /**
+     * 
+     * @return 
+     */
+    String getNaam();
+    
+    /**
+     * 
+     * @return 
+     */
+    double getKoers();
+}
