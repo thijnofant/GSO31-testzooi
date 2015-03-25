8b1d6da032fbf03d5c8ffa70d1d3cef846f068a2
thijnofant <tavandijk@live.nl>
2015-03-25 13:23:23 +0100

    opdracht af?? :smile_cat:

--- RobertThijnAEXBanner/src/Banner/BannerController.java
+++ RobertThijnAEXBanner/src/Banner/BannerController.java
@@ -1,8 +1,9 @@
-/*******  BannerController.java  ***********************************/
-
+/**
+ * ***** BannerController.java  **********************************
+ */
 package Banner;
 
-import Beurs.IEffectenbeurs;
+import Beurs.*;
 import java.util.Timer;
 import java.util.TimerTask;
 import javafx.application.Application;
@@ -10,14 +11,16 @@ import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
-
 public class BannerController extends Application {
 
     private AEXBanner banner;
     private IFonds[] fondsen;
 //  private IEffectenBeurs effectenbeurs;
+    private IEffectenbeurs MockEffectenbeurs;
 
     @Override
+     ;
+
     public void start(Stage primaryStage) {
         banner = new AEXBanner();
         //primaryStage acts as the common stage of the AEXBanner and the 
@@ -31,11 +34,22 @@ public class BannerController extends Application {
 
             @Override
             public void run() {
-                fondsen = IEffectenbeurs.getKoersen();
+                fondsen = MockEffectenbeurs.getKoersen();
+                String temp = "";
+                if (fondsen.length >= 1) {
+                    for (int i = 0; i < fondsen.length; i++) {
+                        temp += fondsen[i].getNaam();
+                        temp += " ";
+                        temp += fondsen[i].getKoers();
+                        temp += " ";
+                    }
+                } else {
+                    temp = "Er zijn op dit moment geen koersen beschikbaar";
+                }
+                banner.setKoersen(temp);
             }
         }, 0, 2000);
 
-        
         //remove pollingTimer as soon as primaryStage is closing:
         primaryStage.setOnCloseRequest((WindowEvent we) -> {
             //pollingTimer.cancel();
