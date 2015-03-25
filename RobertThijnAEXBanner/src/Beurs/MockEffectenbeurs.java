3c5965ca06102007ef6dd9a67b1b6207a1778525
pepperoni <robert.horvers@gmail.com>
2015-03-25 14:05:35 +0100

    tot zo

--- RobertThijnAEXBanner/src/Beurs/MockEffectenbeurs.java
+++ RobertThijnAEXBanner/src/Beurs/MockEffectenbeurs.java
@@ -5,6 +5,7 @@
  */
 package Beurs;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -14,15 +15,16 @@ import java.util.Random;
  */
 public class MockEffectenbeurs implements IEffectenbeurs{
     private IFonds[] koersen;
+    DecimalFormat df = new DecimalFormat("#.00");
     
     public MockEffectenbeurs() {
         koersen = new IFonds[] {
-            new Fonds("Philips", 26.345),
-            new Fonds("KPN", 3.154),
-            new Fonds("Heineken", 71.470),
-            new Fonds("Ahold", 18.325),
-            new Fonds("Shell", 28.485),
-            new Fonds("Unilever", 39.460),
+            new Fonds("Philips", 26.34),
+            new Fonds("KPN", 3.15),
+            new Fonds("Heineken", 71.47),
+            new Fonds("Ahold", 18.32),
+            new Fonds("Shell", 28.48),
+            new Fonds("Unilever", 39.46),
         };
     }
     
@@ -36,8 +38,8 @@ public class MockEffectenbeurs implements IEffectenbeurs{
         for(int i = 0; i < koersen.length; i++) {
             Fonds f = (Fonds)koersen[i];
             Random r = new Random();
-            double verandering = (100 + (-3 +  r.nextInt(6))) / 100;
-            f.setKoers(f.getKoers() * verandering);
+            double verandering = (-1 + r.nextDouble()) / 10 ;
+//            f.setKoers(df.format(f.getKoers() * verandering));
         }
     }
 }
